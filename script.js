7a5af0975561e1692972a473b920c65300200d8a
shashank77665 <shashank77665@gmail.com>
2024-05-31 16:27:31 +0530

    Copy function added

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 
 function generatePassword(){
     let value=document.getElementById("length").value;
-    document.getElementById('password').value = Math.random().toString(36).substr(2, value);
+    let result = Math.random().toString(36).substr(2, parseInt(value));
+    document.getElementById('password').value = result;
+
 }
 
 function reset(){
     document.getElementById('password').value ="";
     document.getElementById('length').value ="";
+}
+
+function copy(){
+    let generatedpass = document.getElementById("password").value;
+    const textarea = document.createElement('textarea');
+    textarea.value = generatedpass;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+    alert("Password Copied")    ;
 }
